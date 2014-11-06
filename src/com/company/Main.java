38700cdee52657ea63d5d6b7631284733a11b027
unknown <daniel.trescak@CZ-NTB-047.ofg.local>
2014-11-06 14:01:50 +0100

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    static class Mutable {
+        int state = 0;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Mutable)) return false;
+
+            Mutable mutable = (Mutable) o;
+
+            if (state != mutable.state) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            return state;
+        }
+
+        @Override
+        public String toString() {
+            return "Mutable{" +
+                    "state=" + state +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Map<Mutable, String> map = new HashMap<>();
+
+        final Mutable mutable = new Mutable();
+        map.put(mutable, "item");
+
+        mutable.state = 1;
+
+        System.out.println(map.get(mutable));
+
+        Set<Map.Entry<Mutable, String>> entries = map.entrySet();
+        for(Map.Entry<Mutable, String> entry : entries) {
+            final Mutable key = entry.getKey();
+            final boolean found = key.equals(mutable);
+            System.out.printf("%s => %s, %s\n", key, entry.getValue(), found);
+        }
+
+    }
+
+}
